7882d30d74e844695776696fd86351f6a79f548b
lam-ngo <lam.ngo.qu@gmail.com>
2018-07-29 00:17:28 +0300

    Modify .babelrc and webpack.config for loading react

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: { main: "./src/index.js" },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    filename: "[name].[chunkhash].js"
   },
   module: {
     rules: [
