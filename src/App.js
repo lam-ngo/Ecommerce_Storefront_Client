ccb5fce094955a58ccf58b7313c5f1d3d9c9d0be
lam-ngo <lam.ngo.qu@gmail.com>
2018-07-29 23:58:33 +0300

    Setup initial Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <BrowserRouter>
+        <div>
+          <NavLink
+            to={'/'}
+            activeStyle={{
+              fontWeight: 'bold',
+              color: 'red'
+            }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={'/shop'}
+            activeStyle={{
+              fontWeight: 'bold',
+              color: 'red'
+            }}
+          >
+            Shop
+          </NavLink>
+          <Route exact path={'/'} component={Home} />
+          <Route path={'/shop'} component={Shop} />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const Shop = () => (
+  <div>
+    <h2>Shop</h2>
+  </div>
+);
 
 export default App;
