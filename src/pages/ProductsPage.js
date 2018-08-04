f90a536643dd1497cf8dd43ce5d79c6fd3c84aeb
lam-ngo <lam.ngo.qu@gmail.com>
2018-08-05 00:49:01 +0300

    Install prop-types and add methods for fetching and rendering products

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,9 +1,76 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import getProductsFromContentful, { client } from '../data/Contentful.config';
 
-const ProductsPage = () => (
-  <div>
-    <h2>Products</h2>
+class ProductsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    getProductsFromContentful().then(products => {
+      console.log(products);
+      this.setState({
+        products: products
+      });
+    });
+  }
+
+  render() {
+    const productsList =
+      this.state.products.length === 0
+        ? 'loading...'
+        : this.state.products.map((product, index) => (
+            <Product key={index} product={product.fields} />
+          ));
+    return (
+      <div>
+        <h2>Products</h2>
+        {productsList}
+      </div>
+    );
+  }
+}
+
+const Product = ({ product }) => (
+  <div style={style.container}>
+    <h3>{product.name}</h3>
+    <p>By {product.brand}</p>
+    <p>
+      <img
+        src={product.image.fields.file.url}
+        alt={product.image.fields.file.fileName}
+      />
+    </p>
+    <p>{product.description}</p>
+    <p>${product.price}</p>
+    <button style={style.button}>Add to cart</button>
+    <hr />
   </div>
 );
+
+const style = {
+  container: {
+    width: '400px',
+    textAlign: 'center'
+  },
+  button: {
+    height: '50px',
+    width: '200px',
+    fontSize: '16px',
+    fontWeight: '600'
+  }
+};
+
+Product.propTypes = {
+  product: PropTypes.object.isRequired
+};
 
 export default ProductsPage;
